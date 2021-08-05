45bd0ef2f3585d6d59a856ac467c58461c5545c0
kenfelix <kmrapper.kf@gmail.com>
2021-08-05 01:12:46 -0700

    New commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - input
+ *
+ * Description: checks if a character is a number
+ * @c: character
+ * Return: 1 yes 0 no
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
