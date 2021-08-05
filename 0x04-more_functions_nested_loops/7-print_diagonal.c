d85a02df4ae3192c6f55388eacaca66aa75a8924
kenfelix <kmrapper.kf@gmail.com>
2021-08-05 05:14:33 -0700

    New commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_line - entry point
+ *
+ * Description: Prints diagonal lines
+ *@n: number of diagonal lines
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int row, num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			if (row > 1)
+			{
+				for (num_lines = 1; num_lines <= row - 1; num_lines++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+
