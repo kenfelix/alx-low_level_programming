8c2c786897ab9b888ad01f3e44c3da6b07d182bf
kenfelix <kmrapper.kf@gmail.com>
2021-08-06 16:17:31 -0700

    My commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,9 +13,9 @@ char *string_toupper(char *s)
 
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s +i) <= 'z')
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			*(s +i) = *(s + i) - 32;
+			*(s + i) = *(s + i) - 32;
 		}
 		i++;
 	}
