794b64e3361e292f8c451a8ce93e8c5a2fa0a0f7
kenfelix <kmrapper.kf@gmail.com>
2021-08-06 16:03:24 -0700

    My commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,26 @@
 #include "holberton.h"
 
 /**
- * _strcmp - compare two strng and returns a value
- *
- * @s1: first string
- * @s2: second string
- *
- * Return: difrence
+ * _strcmp - compare 2 strings.
+ * @s1: string to be compared.
+ * @s2: string for comparation.
+ * Return: Difference.
  */
-
 
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 != '\0' && *s2 != '\0') && *s1 != *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		*s1++
-		*s2++
+		s1++;
+		s2++;
 	}
-
 	if (*s1 == *s2)
 	{
-		return (0)
+		return (0);
 	}
 	else
 	{
-		return (*s1 - *s2)
+	return (*s1 - *s2);
 	}
 }
 
