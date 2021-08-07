981ff61054e4cb54ccd775f68b88f4e2fd6ded92
kenfelix <kmrapper.kf@gmail.com>
2021-08-07 16:07:42 -0700

    My commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: source string
+ * @b: the constant byte for filling
+ * @n: length of buffer
+ * return: new string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	return (s);
+}
+
