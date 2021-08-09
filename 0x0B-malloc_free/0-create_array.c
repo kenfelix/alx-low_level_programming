0cab020ffa429530f8f9d734f046de53f325b835
kenfelix <kmrapper.kf@gmail.com>
2021-08-09 16:30:35 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	memArray = char* malloc(size * sizeof(char));
+	memArray = malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
