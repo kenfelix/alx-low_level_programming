ec1559e20c525835fcc6fbb76d8c79c30f84965f
kenfelix <kmrapper.kf@gmail.com>
2021-08-09 07:51:44 -0700

    commit

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,8 @@
+#ifndef INV_HOLBERTON_H
+#define INV_HOLBERTON_H
+
+int main(int argc __attribute__((unused)), char **argv);
+
+int main(int argc, char **argv);
+
+#endif
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef INV_HOLBERTON_H                              #define INV_HOLBERTON_H
+
+int main(int argc __attribute__((unused)), char **argv);
+
+int main(int argc, char **argv);
+
+#endif
