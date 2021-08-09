99202d7efe1ced5b6fd487869db115bfc29fbdaf
kenfelix <kmrapper.kf@gmail.com>
2021-08-09 16:17:08 -0700

    commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: char for initialized the array.
+ *
+ * Return: the string initialized.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *memArray;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	memArray = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		*(memArray + i) = c;
+	}
+	return (memArray);
+}
+
