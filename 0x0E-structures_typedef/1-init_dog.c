cb24a8a94b3dfc82796660afeaa276d752ec5839
kenfelix <kmrapper.kf@gmail.com>
2021-08-11 11:59:50 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes the struct
+ *
+ * @d: the struct
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: the tags filled
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+	{
+		return;
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
