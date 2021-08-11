8f3f70301359fbe56a0ea5e4b0fccbd74637ce3e
kenfelix <kmrapper.kf@gmail.com>
2021-08-11 12:12:48 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,19 @@ struct dog
 };
 
 /**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  * dog_t - Typedef for struct dog
  */
 
-typedef struct dog dog_t;
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
