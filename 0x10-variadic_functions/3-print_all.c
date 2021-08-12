bb366ea7adc284c2bf5be7466cdcd56764a864ac
kenfelix <kmrapper.kf@gmail.com>
2021-08-12 13:17:42 -0700

    commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,13 +21,13 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(arguments, char));
+			printf("%c", va_arg(arguments, int));
 			break;
 		case 'i':
 			printf("%i", va_arg(arguments, int));
 			break;
 		case 'f':
-			printf("%f", va_arg(arguments, float));
+			printf("%f", va_arg(arguments, double));
 			break;
 		case 's':
 			str = va_arg(arguments, char *);
