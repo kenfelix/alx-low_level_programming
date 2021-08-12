834bc210d81fe4bbd2ebafed0af367c2c4c51f8f
kenfelix <kmrapper.kf@gmail.com>
2021-08-12 03:51:29 -0700

    My commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul -multiplies two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides the first number by the second number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+	else
+	{
+		return (a / b);
+	}
+}
+
+/**
+ * op_mod - obtain the modulus between two.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+	else
+	{
+		return (a % b);
+	}
+}
+
