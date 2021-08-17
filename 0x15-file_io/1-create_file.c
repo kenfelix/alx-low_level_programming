c5f624bbb1ec8737a6a22ffec191a419d9daad33
kenfelix <kmrapper.kf@gmail.com>
2021-08-16 22:56:26 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,10 +26,10 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	}
 
-	for (i = 0; *(file_content + i) != '\0'; i++)
+	for (i = 0; *(text_content + i) != '\0'; i++)
 		;
 
-	write(file, file_content, i);
+	write(file, text_content, i);
 	close(file);
 	return (1);
 }
