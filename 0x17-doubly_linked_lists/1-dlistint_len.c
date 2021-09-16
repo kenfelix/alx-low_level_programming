7471c83dc0dd987844305fa52b1131793fd90f69
kenfelix <kmrapper.kf@gmail.com>
2021-09-16 13:23:06 -0700

    Updates

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,11 +9,14 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
-
+	if (h == NULL)
+	{
+		printf("%s", "Empty list");
+	}
 	while (h != NULL)
 	{
-		i++;
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
