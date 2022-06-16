20551364849c91a45adcd008a3e0e73dbfda6127
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 12:56:16 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""perimeter Module"""
+
+
+def island_perimeter(grid) -> int:
+    """ returns the perimeter of the island described in grid"""
+    new = []
+    for i in grid:
+        for j in i:
+            if j == 1:
+                new.append(j)
+
+    perimeter = 2 + 2 * len(new)
+
+    return perimeter
