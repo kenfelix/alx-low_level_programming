af6d6ea0b1d6ecb043c12455c3b8f853a29e9880
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-06-16 13:07:43 +0100

    Create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
+
+#endif
